af8ea91c4571ab5a79388e39c1714323123bd577
BT_1N3_16 <sample@bths,edu>
2025-01-10 10:50:44 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("our first shared repo");
         System.out.println("Hola!");
-        System.out.println("Bonjour!");
+        System.out.println("gay!");
         System.out.println("i lvoe u");
     }
 

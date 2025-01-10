18ddf03a46f8c5d107047c26d41db56fa5a1914b
BT_1N3_15 <bths@gmai.com>
2025-01-10 10:51:38 -0500

    changed 3rd print

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("our first shared repo");
         System.out.println("Hola!");
-        System.out.println("Bonjour!");
+        System.out.println("Whats up!");
         System.out.println("i lvoe u");
     }
-
-
 }

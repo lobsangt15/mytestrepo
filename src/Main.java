8f1f98d90d78d4d5218bc15bc9d9de15d15d9fc7
BT_1N3_15 <bths@gmai.com>
2025-01-10 10:49:22 -0500

    changed Hello to Hola

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("our first shared repo");
-        System.out.println("Hi");
+        System.out.println("Hola!");
         System.out.println("Bonjour!");
         System.out.println("i hate u ");
     }
